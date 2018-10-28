bf0f32b8dab4f80e489ec844b01b067d60b4f620
adam <chenxuannian@gmail.com>
2018-10-28 21:35:53 +0800

    add vgg architecture graph

--- visualize_architecture.py
+++ visualize_architecture.py
@@ -1,8 +1,12 @@
 # import the necessary packages
 from nn.cnn.lenet import LeNet
 from keras.utils import plot_model
+from keras.applications.vgg16 import VGG16
 
 # initialize LeNet and then write the network architecture
 # visualization graph to disk
-model = LeNet.build(28, 28, 1, 10)
-plot_model(model, to_file="lenet.jpg", show_shapes=True)
+# model = LeNet.build(28, 28, 1, 10)
+# plot_model(model, to_file="lenet.jpg", show_shapes=True)
+
+vgg = VGG16(weights='imagenet')
+plot_model(vgg, to_file='vgg16.jpg', show_shapes=True)
